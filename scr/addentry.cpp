bc8c7cee7d773eeb241ccea0b13de9ec86e530a5
duverleygrajales <s227324@studenti.polito.it>
2018-04-03 19:14:03 +0200

    develop branch lost
    Add the 1 line of code that it had but will need to redo the little modifications of addENtry.ui that dont remember

--- scr/addentry.cpp
+++ scr/addentry.cpp
@@ -168,6 +168,8 @@ void AddEntry::on_InPass_textChanged(QString text){
         ui->InPass2->setEnabled(false);
         ui->sh_pass->setEnabled(false);
         ui->score->setValue(0);
+	ui->entropy->setText("");
+
     }
 }
 
